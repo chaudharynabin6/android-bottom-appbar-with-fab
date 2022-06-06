9d76d2b95dc40ecd151b108227f712a75c1e1944
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-06-07 00:04:39 +0545

    app bar bottom with fab

--- app/src/main/java/com/chaudharynabin6/bottomappbar/MainActivity.kt
+++ app/src/main/java/com/chaudharynabin6/bottomappbar/MainActivity.kt
@@ -2,10 +2,30 @@ package com.chaudharynabin6.bottomappbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.chaudharynabin6.bottomappbar.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+
+            bottomAppBar.setOnMenuItemClickListener(this@MainActivity)
+            fab.setOnClickListener {
+                Log.e("fab ...","fab icon clicked")
+            }
+        }
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        Log.e("menu ...",item.toString())
+        return  true
     }
 }
